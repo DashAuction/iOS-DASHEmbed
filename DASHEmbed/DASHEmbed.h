f40dbe68ecbe4a86a8b2bef744205e15640a9e0e
Ryan Gant <ryga3@hotmail.com>
2018-09-22 15:43:53 -0500

    Reorganized files. Added framework and aggregate targets.

---
+++ DASHEmbed/DASHEmbed.h
@@ -0,0 +1,19 @@
+//
+//  DASHEmbed.h
+//  DASHEmbed
+//
+//  Created by Ryan Gant on 9/10/18.
+//  Copyright © 2018 DASH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DASHEmbed.
+FOUNDATION_EXPORT double DASHEmbedVersionNumber;
+
+//! Project version string for DASHEmbed.
+FOUNDATION_EXPORT const unsigned char DASHEmbedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DASHEmbed/PublicHeader.h>
+#import "DASHEmbed-Swift.h"
+
--- Pods/Target Support Files/Pods-DASHEmbedDemo/Pods-DASHEmbedDemo-resources.sh
+++ Pods/Target Support Files/Pods-DASHEmbedDemo/Pods-DASHEmbedDemo-resources.sh
@@ -91,6 +91,18 @@ EOM
       ;;
   esac
 }
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/DASHEmbed/DASHEmbed.bundle"
+fi
+if [[ "$CONFIGURATION" == "Debug 2" ]]; then
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/DASHEmbed/DASHEmbed.bundle"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/DASHEmbed/DASHEmbed.bundle"
+fi
+if [[ "$CONFIGURATION" == "Release 2" ]]; then
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/DASHEmbed/DASHEmbed.bundle"
+fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
