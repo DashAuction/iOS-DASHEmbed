3c6651f8f3733df650d16d5c0d7773fbc04fb327
Ryan Gant <ryga3@hotmail.com>
2018-09-22 16:46:39 -0500

    Reorganization of files.

--- Pods/Target Support Files/Pods-DASHEmbedDemo/Pods-DASHEmbedDemo-frameworks.sh
+++ Pods/Target Support Files/Pods-DASHEmbedDemo/Pods-DASHEmbedDemo-frameworks.sh
@@ -141,6 +141,19 @@ strip_invalid_archs() {
   STRIP_BINARY_RETVAL=1
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/DASHEmbed/DASHEmbed.framework"
+fi
+if [[ "$CONFIGURATION" == "Debug 2" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/DASHEmbed/DASHEmbed.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/DASHEmbed/DASHEmbed.framework"
+fi
+if [[ "$CONFIGURATION" == "Release 2" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/DASHEmbed/DASHEmbed.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
